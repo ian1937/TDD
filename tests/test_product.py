3e6d6cd577db48fdd7213fadbeb129166060c6a5
ian1937 <iansaputra1234@gmail.comgit config --global user.email iansaputra1234@gmail.comgit config --global user.email iansaputra1234@gmail.com>
2021-01-20 15:09:33 +0700

    split test to classe

--- tests/test_product.py
+++ tests/test_product.py
@@ -5,58 +5,63 @@ from app import app, db
 from app.models import Product
 
 
-@pytest.fixture(scope='session', autouse=True)
-def client():
-    app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-    app.app_context().push()
+class TestBase:
+    @pytest.fixture(scope='session', autouse=True)
+    def client(self):
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+        app.app_context().push()
 
-    with app.test_client() as client:
-        db.create_all()
-        yield client
+        with app.test_client() as client:
+            db.create_all()
+            yield client
 
-    db.drop_all()
+        db.drop_all()
 
-def response_data(client):
-    response = client.get('/')
-    content = response.data
-    return content
+    def response_data(self, client):
+        response = client.get('/')
+        content = response.data
+        return content
 
-@pytest.mark.parametrize("attribute", [
-    b"html",
-    b"head",
-    b"css",
-    b"body",
-    b"form",
-    b"input",
-    b"submit"
-])
 
-def test_has_attribute(client, attribute):
-    assert (attribute) in response_data(client)
+class TestView(TestBase):
+    @pytest.mark.parametrize("attribute", [
+        b"html",
+        b"head",
+        b"css",
+        b"body",
+        b"form",
+        b"input",
+        b"submit"
+    ])
 
-def test_takes_POST_request(client):
-    response = client.post('/', data={'input': 'Say Hello'})
-    assert b"Say Hello" in response_data(client)
+    def test_has_attribute(self, client, attribute):
+        assert (attribute) in self.response_data(client)
 
-def test_database_is_working():
-    product1 = Product(text="First Product")
-    product2 = Product(text="Second Product")
+    def test_takes_POST_request(self, client):
+        response = client.post('/', data={'input': 'Say Hello'})
+        assert b"Say Hello" in self.response_data(client)
 
-    db.session.add(product1)
-    db.session.add(product2)
 
-    db.session.commit()
+class TestDatabase(TestBase):
+    def test_database_is_working(self):
+        product1 = Product(text="First Product")
+        product2 = Product(text="Second Product")
 
-    product_1 = Product.query.filter_by(text="First Product").first()
-    product_2 = Product.query.filter_by(text="Second Product").first()
+        db.session.add(product1)
+        db.session.add(product2)
 
-    assert "First Product" in product_1.text
-    assert "Second Product" in product_2.text
-    
-def test_products_is_saved():
-    assert 3 == Product.query.count()
+        db.session.commit()
 
-def test_data_is_rendered(client):
-    assert b"First Product" in response_data(client)
-    assert b"Second Product" in response_data(client)
+        product_1 = Product.query.filter_by(text="First Product").first()
+        product_2 = Product.query.filter_by(text="Second Product").first()
+
+        assert "First Product" in product_1.text
+        assert "Second Product" in product_2.text
+        
+    def test_products_is_saved(self):
+        assert 3 == Product.query.count()
+
+    def test_data_is_rendered(self, client):
+        assert b"First Product" in self.response_data(client)
+        assert b"Second Product" in self.response_data(client)
